a65807882b7ab215a5f60c4d5ab418102c3d162a
Tom Wells <tom@synthesis.co.za>
2018-12-27 12:52:57 +0200

    decent springboot-react scaffold

--- kotlin/springboot-react/images/[[ .Name ]]-web/src/App.js
+++ kotlin/springboot-react/images/[[ .Name ]]-web/src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  state = {
+    message: ""
+  }
+
+  componentDidMount() {
+    axios.get(`/[[ .Name ]]/greeting`)
+      .then(res => {
+        const message = res.data;
+        this.setState({ message });
+      });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1>[[ .Name ]] Hello world!</h1>
+          <h1>[[ .Name ]] Hello world! ({ this.state.message })</h1>
         </header>
         <div className="App-body">
 
---
+++ kotlin/springboot-react/images/[[ .Name ]]-ws/src/main/kotlin/[[ .Name ]]/Greeting.kt
@@ -0,0 +1,3 @@
+package [[ .Name ]]
+
+data class Greeting(val id: Long, val content: String)
